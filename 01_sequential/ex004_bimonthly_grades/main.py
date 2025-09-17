0cde56b951923f77897092e3955013d9cae31429
Isis Abraham <isiscavalcantiabraham@gmail.com>
2025-09-17 16:22:47 -0400

    Add ex004

---
+++ 01_sequential/ex004_bimonthly_grades/main.py
@@ -0,0 +1,8 @@
+g1 = float(input('Enter grade 1: '))
+g2 = float(input('Enter grade 2: '))
+g3 = float(input('Enter grade 3: '))
+g4 = float(input('Enter grade 4: '))
+
+average = (g1 + g2 + g3 + g4) / 4
+
+print(f'The average is {average}')
