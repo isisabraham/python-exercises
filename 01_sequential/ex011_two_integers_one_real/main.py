743ec8faa2e255ff964fc9dfe5c6a7f8fb1d6b4d
Isis Abraham <isiscavalcantiabraham@gmail.com>
2025-09-17 16:28:11 -0400

    Create main.py

---
+++ 01_sequential/ex011_two_integers_one_real/main.py
@@ -0,0 +1,11 @@
+n1 = int(input('Enter the first integer: '))
+n2 = int(input('Enter the second integer: '))
+n3 = float(input('Enter the real number: '))
+
+a = (2 * n1) * (n2 / 2)
+b = (3 * n1) + n3
+c = n3 ** 3
+
+print(f'a) {a}')
+print(f'b) {b}')
+print(f'c) {c}')
