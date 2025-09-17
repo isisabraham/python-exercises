ca64e99ec7aa9181bf7ffbd6c9ec65808c48b1c8
Isis Abraham <isiscavalcantiabraham@gmail.com>
2025-09-17 16:26:50 -0400

    Create main.py

---
+++ 01_sequential/ex008_salary/main.py
@@ -0,0 +1,4 @@
+hourly_wage = float(input('Enter how much you earn per hour: '))
+hours = float(input('Enter how many hours you worked this month: '))
+salary = hourly_wage * hours
+print(f'Your total monthly salary is {salary}')
