2edab3a81f8237759e3219792f99435e671cff50
Isis Abraham <isiscavalcantiabraham@gmail.com>
2025-09-18 11:12:35 -0400

    Update main.py

--- 02_conditional/ex009_largest_and_smallest/main.py
+++ 02_conditional/ex009_largest_and_smallest/main.py
@@ -1,1 +1,9 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
 
+largest = max(a, b, c)
+smallest = min(a, b, c)
+
+print(f"The largest number is: {largest}")
+print(f"The smallest number is: {smallest}")
